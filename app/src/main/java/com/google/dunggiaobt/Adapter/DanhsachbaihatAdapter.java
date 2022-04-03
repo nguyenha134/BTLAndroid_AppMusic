494ea1ca663c3c0a312467f65c32bcfbf7d69ba9
nguyenha134 <72084153+nguyenha134@users.noreply.github.com>
2022-04-03 22:36:33 +0700

    Update DanhsachbaihatAdapter.java

--- app/src/main/java/com/google/dunggiaobt/Adapter/DanhsachbaihatAdapter.java
+++ app/src/main/java/com/google/dunggiaobt/Adapter/DanhsachbaihatAdapter.java
@@ -26,31 +26,33 @@ public class DanhsachbaihatAdapter extends RecyclerView.Adapter<DanhsachbaihatAd
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater =LayoutInflater.from(context);
-        View view = inflater.inflate(R.layout.dong_danh_sach_bai_hat,parent,false);
+        View view = inflater.inflate(R.layout.activity_danhsachbaihat,parent,false);
         return new ViewHolder(view);
     }
+
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-            BaiHat baihat =mangbaihat.get(position);
+        BaiHat baihat =mangbaihat.get(position);
+        holder.txtcasi.setText(baihat.getCasi());
+        holder.txttenbaihat.setText(baihat.getTenbaihat());
+        holder.txtindex.setText(position+1+"");
 
-            holder.txtcasi.setText(baihat.getCasi());
-            holder.txttenbaihat.setText(baihat.getTenbaihat());
-            holder.txtindex.setText(position+1+"");
     }
+
     @Override
     public int getItemCount() {
         return mangbaihat.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public  class ViewHolder extends RecyclerView.ViewHolder{
         TextView txtindex,txttenbaihat,txtcasi;
         ImageView imgluotthich;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            txtcasi = itemView.findViewById(R.id.textviewtencasi);
+            txtcasi = itemView.findViewById(R.id.texviewtencasi);
             txtindex =itemView.findViewById(R.id.textviewdanhsachindex);
             txttenbaihat =itemView.findViewById(R.id.textviewtenbaihat);
-            imgluotthich = itemView.findViewById(R.id.imageviewluotthich);
+            imgluotthich = itemView.findViewById(R.id.imageviewluotthichdanhsachbaihat);
         }
     }
 }
